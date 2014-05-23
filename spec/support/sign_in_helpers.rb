3901d1462f404c1ca8a2006f1cbfc1085f4a72a3
Feather <lisafeather@gmail.com>
2014-05-22 17:55:55 -0700

    test failing when expected, wrong user

--- spec/support/sign_in_helpers.rb
+++ spec/support/sign_in_helpers.rb
@@ -1,10 +1,13 @@
 module SignInHelpers
   def sign_in
+    visit root_path
+    fill_in 'Email address', with: 'person@example.com'
+    click_button 'Sign In'
   end
 
   def sign_in_as(email)
     visit root_path
-    fill_in 'Email address', with: 'person@example.com'
+    fill_in 'Email address', with: email
     click_button 'Sign In'
   end
 
